c1f091b93538ca8e185db062d994f06bfe146719
CDaKhaos <13982121663@sina.cn>
2022-07-25 15:25:21 +0800

    cc_platform change OS MACO

--- cpp/CXML/CXML/cc_platform.hpp
+++ cpp/CXML/CXML/cc_platform.hpp
@@ -3,9 +3,6 @@
 #ifndef __CC_PLATFORM_H__
 #define __CC_PLATFORM_H__
 
-
-#define LINUX
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,9 +10,9 @@
 template< class T >
 inline void cc_sprintf(char* const buffer, char const* const _format, const T& value, size_t iBufSize = 2048)
 {
-#ifdef LINUX
+#ifdef __linux__
 	sprintf(buffer, _format, value);
-#else
+#elif _WIN32
 	sprintf_s(buffer, iBufSize, _format, value);
 #endif
 }
@@ -23,36 +20,36 @@ inline void cc_sprintf(char* const buffer, char const* const _format, const T& v
 template< class T >
 inline int cc_sscanf(char const* const _Buffer, char const* const _Format, T* pValue)
 {
-#ifdef LINUX
+#ifdef __linux__
 	return sscanf(_Buffer, _Format, pValue);
-#else
+#elif _WIN32
 	return sscanf_s(_Buffer, _Format, pValue);
 #endif
 }
 
 inline int cc_strcmp(char const* _String1, char const* _String2)
 {
-#ifdef LINUX
+#ifdef __linux__
 	return strcmp(_String1, _String2);
-#else
+#elif _WIN32
 	return strcmp(_String1, _String2);
 #endif
 }
 
 inline void cc_strcpy(char* _String1, char const* _String2, size_t iSize)
 {
-#ifdef LINUX
+#ifdef __linux__
 	strcpy(_String1, _String2);
-#else
+#elif _WIN32
 	strcpy_s(_String1, iSize, _String2);
 #endif
 }
 
 inline void cc_fopen(FILE **fp, char const* _FileName, char const* _Mode)
 {
-#ifdef LINUX
+#ifdef __linux__
 	*fp = fopen(_FileName, _Mode);
-#else
+#elif _WIN32
 	fopen_s(fp, _FileName, _Mode);
 #endif
 }
